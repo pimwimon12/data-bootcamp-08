496eb2de8db34aa7adf61ae2bb74994e7e045c7c
Pimwimon <141467624+pimwimon12@users.noreply.github.com>
2023-09-29 16:50:23 +0700

    Update RPSgame (Pao-ying-chup. game).r

--- R/RPSgame (Pao-ying-chup. game).r
+++ R/RPSgame (Pao-ying-chup. game).r
@@ -1,17 +1,16 @@
 RPS_game <- function() {
   RPS <- c("rock","paper","scissors")
   
-  count <- -1
+  count <- 0
   win <- 0
   draw<- 0
   lose <- 0
   
   while(TRUE) {
     com_input <- sample(RPS, 1)
-    player_input <- readline("Input rock/paper/scissors or input X for end game")
-    count = count+1
+    player_input <- readline("Input rock/paper/scissors or Input X for End Game: ")
     
-    if (player_input == "X") {print("End Game")
+    if (player_input == "X" | player_input == "x") {print("End Game")
       print(paste("You play ",count,"game"))
       print(paste("win: ",win))
       print(paste("Draw: ",draw))
@@ -19,15 +18,26 @@ RPS_game <- function() {
       break}
     
     else if (player_input == com_input) {print(paste("You:",player_input,"| Computer:",com_input,"->","It's draw"))
-      draw = draw+1 }
+      count = count+1
+      draw = draw+1}
     else if (player_input == "rock" & com_input == "scissors") {print(paste("You:",player_input,"| Computer:",com_input,"->","You Win"))
+      count = count+1
       win = win+1}
     else if (player_input == "paper" & com_input == "rock") {print(paste("You:",player_input,"| Computer:",com_input,"->","You Win"))
+      count = count+1
       win = win+1}
     else if (player_input == "scissors" & com_input == "paper") {print(paste("You:",player_input,"| Computer:",com_input,"->","You Win"))
+      count = count+1
       win = win+1}
-    else {print(paste("You:",player_input,"| Computer:",com_input,"->","You Lose"))
-      lose = lose+1}  
-}}
+    else if (player_input == "rock" & com_input == "paper") {print(paste("You:",player_input,"| Computer:",com_input,"->","You Lose"))
+      count = count+1
+      lose = lose+1}
+    else if (player_input == "paper" & com_input == "scissors") {print(paste("You:",player_input,"| Computer:",com_input,"->","You Lose"))
+      count = count+1
+      lose = lose+1}
+    else if (player_input == "scissors" & com_input == "rock") {print(paste("You:",player_input,"| Computer:",com_input,"->","You Lose"))
+      count = count+1
+      lose = lose+1}
+    else {print("Please input 'rock'/'paper'/'scissors' or 'X' only")}  }}
 
 RPS_game()
